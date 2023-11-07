7bfafadcd62b55dfff3f87d2847c9d04c9fa7837
Amelie <amelie.barre@cpexterne.org>
2023-11-07 14:27:46 +0100

    create footer and display data

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,8 +25,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" fill className="object-contain" alt="hero" />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
