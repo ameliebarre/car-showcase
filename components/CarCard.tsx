a2df55dd9ae64ab067af6988a7409c136ec1ae20
Amelie <amelie.barre@cpexterne.org>
2023-11-10 22:00:24 +0100

    add cars assets

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -16,27 +16,33 @@ const CarCard = ({ car }: CarCardProps) => {
   const carRent = calculateCarRent(city_mpg, year);
   const [isOpen, setIsOpen] = useState(false);
 
+  const modelLabel = model
+    .split(" ")
+    .map((item) => (item.length <= 3 ? item.toUpperCase() : item))
+    .join(" ");
+
   return (
     <div className="car-card group">
-      <div className="car-card__content">
+      <div className="relative w-full h-40 my-3">
+        <Image
+          src={`/cars/${make}_${model}.jpg`}
+          alt="car model"
+          fill
+          priority
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="car-card__content flex flex-col">
         <h2 className="car-card__content-title">
-          {make} {model}
+          {make} {modelLabel}
         </h2>
+        <span className="text-gray-800">{year}</span>
       </div>
       <p className="flex mt-6 text-[32px] font-extrabold">
         <span className="self-start text-[14px] font-semibold">$</span>
         {carRent}
         <span className="self-center text-[14px] font-medium">/day</span>
       </p>
-      <div className="relative w-full h-40 my-3 object-contain">
-        <Image
-          src="/hero.png"
-          alt="car model"
-          fill
-          priority
-          className="object-contain"
-        />
-      </div>
       <div className="relative flex w-full mt-2">
         <div className="flex group-hover:invisible w-full justify-between text-gray">
           <div className="flex flex-col justify-center items-center gap-2">
